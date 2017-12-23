3f30120cd3659f073a8a6ad35e1ed1fa2e627ad1
Rajat Sharma <lunasunkaiser@gmail.com>
2017-12-24 02:10:25 +0530

    Removed Private Keys from build

--- packages/build/index.js
+++ packages/build/index.js
@@ -4,6 +4,5 @@ const wpmerge = require('webpack-merge')
 // Webpack config as a function of env
 module.exports = (env, entry, output) => {
   const envConfig = require(`./webpack.${env.env}`)
-  console.log(wpmerge(commonConfig(entry, output), envConfig))
   return wpmerge(commonConfig(entry, output), envConfig)
 }
