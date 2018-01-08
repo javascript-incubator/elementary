268e31b8eb61d929811698938966b00ff8ac41f1
Rajat Sharma <lunasunkaiser@gmail.com>
2018-01-08 12:47:39 +0530

    Release 0.1.1

--- packages/standard/src/border.js
+++ packages/standard/src/border.js
@@ -1,9 +1,10 @@
 const borderStylePropFrame = dir => `border${dir}Style`
 const borderWidthPropFrame = dir => `border${dir}Width`
 const borderColorPropFrame = dir => `border${dir}Color`
+const borderOnly = dir => `border${dir}`
 
 const directions = ['Top', 'Bottom', 'Left', 'Right', '']
 
-const borderStyleGroupFrame = directions.map(borderStylePropFrame).concat(directions.map(borderWidthPropFrame)).concat(directions.map(borderColorPropFrame)).concat(['border'])
+const borderStyleGroupFrame = directions.map(borderStylePropFrame).concat(directions.map(borderWidthPropFrame)).concat(directions.map(borderColorPropFrame)).concat(directions.map(borderOnly))
 
 export default borderStyleGroupFrame
