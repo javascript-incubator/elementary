1b0e74d765b5222d1c2b5cb5acfee8e082df481b
Rajat Sharma <lunasunkaiser@gmail.com>
2018-02-24 05:43:39 +0530

    Added coyoneda

--- packages/lab/test.js
+++ packages/lab/test.js
@@ -1,3 +1,26 @@
-const { toValueArray } = require('../proper/lib/main')
+// import test from 'ava'
 
-console.log(toValueArray({ c: 89 }))
+const { ccompose } = require('../core/lib/main')
+
+const map = f => object => object.map(f)
+
+const object = val => {
+  console.log('logged')
+  return {
+    map: (f) => {
+      return object(f(val))
+    },
+    prj: () => val
+  }
+}
+
+console.log(ccompose(
+  x => 3+x,
+  x => 4+x,
+  x => 5+x
+)(object(4)).prj())
+
+// test(t => t.is(
+//   String(compose(map(x => 3+x), map(x => 4+x), map(x => 5+x)(object(4)))),
+//   16
+// ))
