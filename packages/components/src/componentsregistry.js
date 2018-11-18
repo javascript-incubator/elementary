7f2ee88d781db2bb106a5b40fbc4c30c016c3494
Rajat Sharma <rajatsharma0720@gmail.com>
2018-11-19 04:27:21 +0530

    Upgrading components

--- packages/components/src/componentsregistry.js
+++ packages/components/src/componentsregistry.js
@@ -34,16 +34,13 @@ const components = [
     name: 'ButtonOutline',
     type: 'Button',
     props: {
-      color: 'black',
       bg: 'transparent',
       boxShadow: `inset 0 0 0 2px`,
       hover: props => ({
-        color: 'white',
         backgroundColor: props.color,
       }),
       focus: { boxShadow: `inset 0 0 0 2px, 0 0 0 2px` },
       active: props => ({
-        color: 'white',
         backgroundColor: props.color,
         boxShadow: `inset 0 0 0 2px ${props.color}, inset 0 0 8px ${darken(
           1 / 4,
