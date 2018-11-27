75047c010066e3e73aa19a6454de2ff6fb661f0a
Rajat Sharma <rajatsharma0720@gmail.com>
2018-11-27 20:43:15 +0530

    Using internal components

--- packages/components/src/styletransformer.js
+++ packages/components/src/styletransformer.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import React from 'react'
+
 import styles, {
   props as blacklist,
   space,
@@ -14,11 +16,47 @@ import styles, {
 } from '@elementary/standard/lib/styles'
 
 import { oneOfType, number, string, func, object } from 'prop-types'
-import { createElement, propHunter } from '@elementary/transformers'
 
 const prop = oneOfType([number, string, func, object])
 
 const propTypes = props.reduce((acc, x) => ({ ...acc, [x]: prop }), {})
+
+const createElement = type => {
+  const Base = props => {
+    const isEl = typeof type === 'string'
+    const Comp = isEl ? props.is || type : type
+    const next = { ...props }
+
+    if (isEl) delete next.is
+
+    return <Comp {...next} />
+  }
+
+  Base.displayName =
+    typeof type === 'string' ? `Created(${type})` : 'CreateElementWrapper'
+
+  Base.propTypes = {
+    is: string,
+  }
+
+  return Base
+}
+
+const propHunter = (blacklist = []) => Com => {
+  const clean = huntProps(blacklist)
+  const PropHunterElement = props => <Com {...clean(props)} />
+  PropHunterElement.displayName = 'PropHunterElement'
+  return PropHunterElement
+}
+
+export const huntProps = blacklist => props => {
+  const next = {}
+  for (let key in props) {
+    if (blacklist.includes(key)) continue
+    next[key] = props[key]
+  }
+  return next
+}
 
 const withStyle = (style, props, extras = []) => Component => {
   const Base = styled(Component)(
