c8a0cb0befb0d825d6e9cae56e2d0c7f9bbed42a
Rajat Sharma <rajatsharma0720@gmail.com>
2018-12-06 06:49:51 +0530

    Added docs

---
+++ packages/docs/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/rajatsharma/Desktop/elementary/packages/docs/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/home/rajatsharma/Desktop/elementary/packages/docs/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ packages/docs/.cache/sync-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/rajatsharma/Desktop/elementary/packages/docs/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/rajatsharma/Desktop/elementary/packages/docs/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/rajatsharma/Desktop/elementary/packages/docs/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/home/rajatsharma/Desktop/elementary/packages/docs/src/pages/page-2.js"))
+}
+
