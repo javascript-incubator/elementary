452ee43c7db229633b99f1e3c0327c28cc3b037d
Rajat Sharma <lunasunkaiser@gmail.com>
2018-12-19 22:34:42 +0530

    Changed build method

--- packages/proper/build.js
+++ packages/proper/build.js
@@ -1,4 +1,18 @@
-const buildConfig = require('@elementary/charger')
-const paths = require('./paths')
+const Lectro = require('@lectro/core');
+const BuildUtils = require('@lectro/enhancer-buildutils');
+const path = require('path');
 
-buildConfig(paths)
+new Lectro()
+  .use(BuildUtils)
+  .setEntry(path.resolve(__dirname, './', 'src', 'index.js'))
+  .tap(
+    self =>
+      (self.output = {
+        library: '@elementary/proper',
+        path: path.resolve(__dirname, './', 'lib'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2',
+      }),
+  )
+  .devtool('none')
+  .build();
