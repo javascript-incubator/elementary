74389fcd0ae78be528423a8c5b1921c163b58488
Rajat Sharma <rajatsharma0720@gmail.com>
2018-12-31 12:30:26 +0530

    Fixed eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = require('@hellpack/eslint')({
   rules: {
     quotes: ['error', 'single'],
     'react/jsx-filename-extension': 0,
+    'import/no-extraneous-dependencies': 0,
   },
 });
