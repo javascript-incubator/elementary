8ac0b9c79d29b92f42dd48cc53da5c0ebaecddf2
Rajat Sharma <rajatsharma0720@gmail.com>
2019-01-18 22:46:12 +0530

    Published to 0.1.1-alpha.1

--- packages/modalcontroller/src/index.js
+++ packages/modalcontroller/src/index.js
@@ -2,9 +2,10 @@ import React, { createContext, Component } from 'react';
 import ReactDOM from 'react-dom';
 
 const ModalContext = createContext('modal');
+/* eslint-disable one-var */
 
 const uuidv4 = () =>
-  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     let r = (Math.random() * 16) | 0,
       v = c == 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
