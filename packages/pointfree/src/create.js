cf716cf7f2160ddd062aa6667572ee1597f0a894
Rajat Sharma <rajatsharma0720@gmail.com>
2019-04-13 12:05:49 +0530

    Removed elementary extras

--- packages/pointfree/src/create.js
+++
@@ -1,9 +0,0 @@
-const create = method => (...args) => object => {
-  if (!object[method]) {
-    throw new Error(method + ' not implemented');
-  }
-
-  return object[method](...args);
-};
-
-export default create;
--- packages/transformers/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as propHunter } from './prophunter';
-export { default as IdentityComponent } from './identitycomponent';
-export { default as createElement } from './createelement';
