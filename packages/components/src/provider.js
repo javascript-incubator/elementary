a4883dcd5ad5a327b047fdecfb323e8ce1a543ff
Rajat Sharma <rajatsharma0720@gmail.com>
2019-04-16 12:39:49 +0530

    Fixed entry file

--- packages/components/src/provider.js
+++ packages/components/src/provider.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { themeTransformer } from '@elementary/standard';
-
 import { ThemeProvider } from 'emotion-theming';
 import {
   shape,
