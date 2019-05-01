b6ecf2957656b1e84ae5851628e5c2a6d1a05694
Rajat Sharma <rajatsharma0720@gmail.com>
2019-05-01 16:27:07 +0530

    Standard Testing

--- packages/standard/__tests__/index.js
+++ packages/standard/__tests__/index.js
@@ -3,15 +3,17 @@ import test from 'ava';
 const { themeTransformer } = require('../build/main');
 const Standard = require('../build/main');
 
-const { breakpoints } = themeTransformer({ breakpoints: ['md'] });
+const { breakpoints } = themeTransformer({ breakpoints: ['700px'] });
 
-Standard.flexContainer({
-  justifyContent: breakpoints(['md', 'sm']),
-  alignItems: breakpoints(['md', 'sm']),
+test('Generated style', t => {
+  const style = Standard.flexContainer({
+    justifyContent: breakpoints(['center', 'left']),
+    alignItems: breakpoints(['center', 'left']),
+  });
+
+  t.deepEqual(style, {
+    justifyContent: 'center',
+    alignItems: 'center',
+    '700px': { justifyContent: 'left', alignItems: 'left' },
+  });
 });
-
-test('Dummy Test', t => t.pass());
-
-// console.log(
-//
-// );
